8c9ef56923e0b47750f2753b5c47c43ff965dd3e
Hugo V <hugovedrine@hotmail.com>
2024-08-17 18:09:26 +0200

    Fix generation eor et lb

--- app/model/connecteur/WarRequest.scala
+++ app/model/connecteur/WarRequest.scala
@@ -33,12 +33,12 @@ object WarRequestRayshift extends WarRequest {
         val quests: List[String] = (resultCall \ "mstQuest").get.as[List[JsValue]].map(x=>(x \ "allScripts" ).get.as[List[JsValue]])
             .filter(x => !x.isEmpty).flatMap(x=>x.map(y => (y \ "scriptFileName").get.toString)).map(s => s.substring(1, s.length()-1))
             .map(s=> s match {
-                case x if x.charAt(0)=='0' => x.substring(1)
+                case x if x.charAt(0)=='0' && x.charAt(1) =='1' => x.substring(1)
                 case _: String => s
             })
         intro match {
             case NONE => quests
-            case _ if intro.charAt(1)=='0' => intro.substring(2, intro.length()-1)::quests
+            case _ if intro.charAt(1)=='0' && intro.charAt(2) =='1' => intro.substring(2, intro.length()-1)::quests
             case _  => intro.substring(1, intro.length()-1)::quests
         }
     }
