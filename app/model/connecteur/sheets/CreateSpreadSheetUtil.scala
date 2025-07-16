7dde9d79ed7b70f204964df1f19f4015285d1720
Hugo V <hugovedrine@hotmail.com>
2025-07-16 10:24:29 +0200

    Rename first sheet MASTER

--- app/model/connecteur/sheets/CreateSpreadSheetUtil.scala
+++ app/model/connecteur/sheets/CreateSpreadSheetUtil.scala
@@ -4,6 +4,9 @@ import com.google.api.services.sheets.v4.model.Spreadsheet
 import com.google.api.services.sheets.v4.model.SpreadsheetProperties
 import model.exception.ConnexionException
 import play.api.Logging
+import com.google.api.services.sheets.v4.model.Sheet
+import com.google.api.services.sheets.v4.model.SheetProperties
+import java.{util => ju}
 
 trait SpreadSheetUtilAbstractForm {
     val sheetService = SheetsServiceUtil.getSheetsService
@@ -12,7 +15,8 @@ trait SpreadSheetUtilAbstractForm {
 
 object SpreadSheetUtil extends SpreadSheetUtilAbstractForm with ExecutionSheet with Logging {
     override def createSpreadSheet(idWar: String): Spreadsheet = {
-        val spreadSheet = new Spreadsheet().setProperties(new SpreadsheetProperties().setTitle(idWar))
+        val masterSheet = new Sheet().setProperties(new SheetProperties().setTitle("MASTER"))
+        val spreadSheet = new Spreadsheet().setProperties(new SpreadsheetProperties().setTitle(idWar)).setSheets(ju.List.of(masterSheet))
         execute(sheetService.spreadsheets().create(spreadSheet)) match {
             case Some(x) => logger.info(s"Spreadsheet créé $x");x
             case None => throw new ConnexionException(s"Création échoué pour war = $idWar")
